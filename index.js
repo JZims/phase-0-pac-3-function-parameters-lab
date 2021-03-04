d86b3318eaab49db7380668db27c7f118732e75f
JZims <JZimmsGames@gmail.com>
2021-03-04 16:49:24 -0500

    Added default value for 'name'

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function introductionWithLanguage(name, language) {
  return `Hi, my name is ${name} and I am learning to program in ${language}.`;   
 }
 
-function introductionWithLanguageOptional(name, language="JavaScript") {
+function introductionWithLanguageOptional(name = "Josh", language="JavaScript") {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
